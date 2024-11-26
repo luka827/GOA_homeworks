d702e49b409321ccbafed4019f87c9ca6613bd99
luka827 <gogaladzeluka867@gmail.com>
2024-11-26 20:57:49 +0400

    eight commit

---
+++ day.08/homework/hw3.py
@@ -0,0 +1,4 @@
+name = input("what is your name ?? ")
+age = input ("how old are you ?? ")
+lastname = input( "what is youer lastname ?")
+print("hello  " + name + age + lastname + " , nise to meat you.\n\n\n")
