bbb11e92c9b333af5b5bd16a38a7f17de77b6507
luka827 <gogaladzeluka867@gmail.com>
2025-01-09 21:05:05 +0400

    commit

---
+++ day.12/homework/hw1.py
@@ -0,0 +1,16 @@
+name="My name is Luka"
+surname="my last name is Gogaladze"
+age="I'm 16 years old"
+sport="mma"
+sport="football"
+age1=16
+age2=17
+age3=18 
+age4=26
+age5=27
+
+heigh=1.89
+heigh1=1.95
+heigh2=1.65
+heigh3=1.75
+heigh4=2.85
---
+++ day.12/homework/hw5.py
@@ -0,0 +1,31 @@
+x = 5
+y = 3
+print(x > y)  # True
+
+x = 5
+y = 8
+print(x < y)  # True
+
+
+
+x = 10
+y = 10
+print(x >= y)  # True
+
+
+
+x = 4
+y = 7
+print(x <= y)  # True
+
+
+
+x = 5
+y = 6
+print(x != y)  # True
+
+
+
+x = 2
+y = 2
+print(x == y)  # True
