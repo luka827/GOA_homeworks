c86e3c898d08d19ba1e20af2966256e7ed9d5d9f
luka827 <gogaladzeluka867@gmail.com>
2025-01-29 19:53:00 +0400

    commit

---
+++ day.15/classwork/cw1.py
@@ -0,0 +1,13 @@
+print (2>7 and 77>4)
+print (245>98 and 15>96)
+print (7>5 and 5>2)
+print (9>7 and 5>9)
+
+
+
+
+
+print (78>52 or 85>55)
+print (3>2 or 59>20)
+print (79>51 or 65>20)
+print (12>11 or 9>13)
