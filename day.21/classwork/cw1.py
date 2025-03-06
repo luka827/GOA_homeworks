15163cb148a6d730a5a46cecb8dc5f0f34aabea9
luka827 <gogaladzeluka867@gmail.com>
2025-03-06 18:10:47 +0400

    commit

---
+++ day.21/classwork/cw1.py
@@ -0,0 +1,18 @@
+
+cars = ["BMW", "Ferrari", "AMG GT", "SLS AMG", "AMG One"]
+print(cars)
+foods = ["ხინკალი", "მწვადი", "ქაბაბი", "ტოლმა"]
+print(foods)
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+
+
+
+print ( num[1:8])
+   
+
+
+
+print (num[4:10]) 
+  
